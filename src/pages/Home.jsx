5ce563c2ab4c0d0fb51584cbdb070dfa31c7092a
Santiago <dellasala1992@gmail.com>
2025-05-23 23:25:56 -0300

    Agregado fondo negro al home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,29 @@
 function Home() {
+  const style = {
+    position: 'relative',
+    padding: '2rem',
+    height: '100vh',
+    color: 'white',
+    backgroundColor: 'black',
+    overflow: 'hidden',
+  };
+
+  const backgroundStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: 'url(/fondo.jpg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    opacity: 0.3, // opacidad para que se vea el negro debajo
+    zIndex: -1,
+  };
+
   return (
-    <main style={{ padding: '2rem' }}>
+    <main style={style}>
+      <div style={backgroundStyle}></div>
       <h1>Bienvenido a Durnom</h1>
       <p>Noticias del desarrollo aparecerán aquí.</p>
     </main>
